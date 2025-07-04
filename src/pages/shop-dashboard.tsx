845ac00960f28b2045185054a38cf6a3d0a531de
unmatched78 <marvinavi24@gmail.com>
2025-07-05 00:02:00 +0200

    feat: separate  dashbaord acoording to the User-role

--- src/pages/shop-dashboard.tsx
+++ src/pages/shop-dashboard.tsx
@@ -19,7 +19,7 @@ interface HomeProps {
   userName?: string;
 }
 
-const Home = ({ userRole = "customer", userName = "John Doe" }: HomeProps) => {
+const Home = ({ userRole = "veterinarian", userName = "John Doe" }: HomeProps) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const [activeTab, setActiveTab] = useState("overview");
 
