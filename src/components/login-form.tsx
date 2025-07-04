e69d1c71cd3e5d5c184599f04020fb24ff714211
Viella <168560797+unmatched78@users.noreply.github.com>
2025-07-04 16:33:46 +0000

    feat: update image source in LoginForm for enhanced visual appeal

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -139,7 +139,7 @@ export function LoginForm({
           </form>
           <div className="bg-muted relative hidden md:block">
             <img
-              src="/placeholder.svg"
+              src="https://images.unsplash.com/photo-1541364983171-a8ba01e95cfc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnVubnl8ZW58MHx8MHx8fDA%3D"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
