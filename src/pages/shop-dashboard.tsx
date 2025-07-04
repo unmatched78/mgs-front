7ff097e76793abf13a55e4bb48e223543118e186
unmatched78 <marvinavi24@gmail.com>
2025-07-04 23:57:55 +0200

    feat: separate  dashbaord acoording to the User-role

--- src/pages/shop-dashboard.tsx
+++ src/pages/shop-dashboard.tsx
@@ -16,7 +16,7 @@ type UserRole = "staff" | "supplier" | "veterinarian" | "customer";
 
 interface HomeProps {
   userRole?: UserRole;
-  userName?: string;m
+  userName?: string;
 }
 
 const Home = ({ userRole = "supplier", userName = "John Doe" }: HomeProps) => {
